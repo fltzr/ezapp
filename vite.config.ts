62115e59dc97e61dc8329b8614801b43a6c3585b
fltzr <josh.salinas.j@outlook.com>
2024-04-28 22:57:45 -0400

    chore: optimize build output file names and paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,46 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: ({ name }) => {
+          console.info(`nameCf: ${JSON.stringify(name, null, 2)}`);
+
+          if (/all\..+\.$/.test(name)) {
+            return 'locales/[name]-[hash][extname]';
+          }
+
+          return 'chunks/[name]-[hash].js';
+        },
+        entryFileNames: ({ name }) => {
+          console.info(`nameEf: ${JSON.stringify(name, null, 2)}`);
+
+          return 'entry/[name]-[hash].js';
+        },
+        assetFileNames: ({ name }) => {
+          console.info(`name: ${JSON.stringify(name, null, 2)}`);
+
+          if (!name) {
+            return 'assets/[name]-[hash][extname]';
+          }
+
+          if (name.match(/^all\..*\.js$/)) {
+            return 'locales/[name]-[hash][extname]';
+          }
+
+          if (/\.(gif|jpe?g|png|svg)/.test(name)) {
+            return 'images/[name]-[hash][extname]';
+          }
+
+          if (/\.(css)$/.test(name)) {
+            return 'styles/[name]-[hash][extname]';
+          }
+
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
 });
