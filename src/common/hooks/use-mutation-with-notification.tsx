30469543d3889e12ef589decee656e654f17a9cd
fltzr <josh.salinas.j@outlook.com>
2024-05-07 07:14:15 -0400

    chore: Refactor useMutationWithNotification hook
    The useMutationWithNotification hook has been refactored to improve code readability and maintainability. The unnecessary commented code has been removed, and the code has been reorganized for better structure. This refactoring will make it easier to understand and maintain the hook in the future.

--- src/common/hooks/use-mutation-with-notification.tsx
+++ src/common/hooks/use-mutation-with-notification.tsx
@@ -1,47 +1,11 @@
-// import { useMutation, useQueryClient, type UseMutationOptions } from '@tanstack/react-query';
-// import type { AxiosError } from 'axios';
-// import { nanoid } from 'nanoid';
-// import { useNotificationStore } from '@/store/use-notification-store';
+import { MutationFunction, UseMutationOptions } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
-// //https://chat.openai.com/share/9de00e1c-8eab-4d5a-9f70-15cf88628448
+type UseMutationWithNotificationProps<TData, TVariables, TError = AxiosError> = 
+  Omit<UseMutationOptions<TData, TVariables, TError>, 'mutationFn'> & MutationFunction<TData, TVariables>;
 
-// type MutationOptions<TData, TVariables> = UseMutationOptions<TData, AxiosError, TVariables> & {
-//   successMessage?: string;
-//   errorMessage?: string;
-// };
+export const useMutationWithNotification = <TData, TVariables, TError = AxiosError>({
+  mutationFn: 
+}: UseMutationWithNotificationProps<TData, TVariables, TError>) => {
 
-// type UseMutationWithNotificationProps<TData, TVariables> = {
-//   mutationFn: (variables: TVariables) => Promise<TData>;
-//   options?: MutationOptions<TData, TVariables>;
-// };
-
-// export const useMutationWithNotification = <TData, TVariables>({
-//   mutationFn,
-//   options,
-// }: UseMutationWithNotificationProps<TData, TVariables>) => {
-//   const queryClient = useQueryClient();
-//   const { addNotification, removeNotification } = useNotificationStore();
-
-//   const mutation = useMutation<TData, AxiosError, TVariables>({
-//     mutationFn,
-//     ...options,
-//     onSuccess: (data, variables, context) => {
-//       if (options?.successMessage) {
-//         addNotification({
-//           id: nanoid(11),
-//           type: 'success',
-//           header: options.successMessage,
-//         });
-//       }
-//       options?.onSuccess?.(data, variables, context);
-//     },
-//     onError: (error, variables, context) => {
-//       const uid = nanoid(11);
-
-//       addNotification({
-//         id: uid,
-//         type: 'error',
-//       });
-//     },
-//   });
-// };
+}
